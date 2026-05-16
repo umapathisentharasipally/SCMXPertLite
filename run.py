e8b68bbb25b5c6d5bc44ef5632ae4a0fd40e167f
Tech Support _ Umapathi S <Techsupport.umapathi@exafluence.com>
2026-05-16 11:20:12 +0530

    sign up front end

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
+import uuid
+from datetime import datetime, timezone
+from passlib.context import CryptContext
 from pymongo import MongoClient
-from urllib.parse import quote_plus
+ 
+from back_end.db.database import get_db
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-username = "SCMX_PERT_LITE"
-password = quote_plus("Scmx123")
+db = get_db()
 
-url = (
-    f"mongodb+srv://{username}:{password}"
-    f"@users.qarlknd.mongodb.net/"
-)
+user_id = str(uuid.uuid4())
 
-client = MongoClient(url)
+db.users.insert_one({
+    "id": user_id,
+    "full_name": "super_admin",
+    "email": "sentharasipallyumapathi@gmail.com",
+    "phone_number": "9398860744",
+    "hashed_password": pwd.hash("Super@123"),
+    "role": "super_admin",
+    "is_active": True,
+    "created_by": None,
+    "admin_id": None,
+    "created_at": datetime.now(timezone.utc),
+    "updated_at": datetime.now(timezone.utc)
+})
 
-print(client.list_database_names())
+print("Super admin created")
